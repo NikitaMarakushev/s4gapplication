a90e791696c1ae9decb5a2dd630b943630626055
NikitaMarakushev <nikitamarakushev@yandex.ru>
2021-09-08 20:42:16 +0300

    initial

---
+++ src/main/java/com/example/frnicky_spring_test/FrnickySpringTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.frnicky_spring_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrnickySpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrnickySpringTestApplication.class, args);
+	}
+
+}
