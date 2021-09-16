cf10e05ab1b35aea4d65419fb560ce8d3bbd3ede
NikitaMarakushev <nikitamarakushev@yandex.ru>
2021-09-16 15:45:04 +0300

    get data difference for certain task

--- src/main/java/com/example/frnicky_spring_test/entity/TaskEntity.java
+++ src/main/java/com/example/frnicky_spring_test/entity/TaskEntity.java
@@ -2,6 +2,7 @@ package com.example.frnicky_spring_test.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.util.Date;
 
 @Entity
 @Table(name = "task")
@@ -9,8 +10,9 @@ public class TaskEntity {
 
     private String taskName;
 
-    public TaskEntity() {
+    private Date doneUntil;
 
+    public TaskEntity() {
     }
 
     public String getTaskName() {
@@ -19,5 +21,13 @@ public class TaskEntity {
 
     public void setTaskName(String taskName) {
         this.taskName = taskName;
+    }
+
+    public void setDoneUntil(Date doneUntil) {
+        this.doneUntil = doneUntil;
+    }
+
+    public Date getRestTimeToDeadline() {
+        return new Date(this.doneUntil.getTime() - ((new Date()).getTime()));
     }
 }
